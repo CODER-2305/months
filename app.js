e2c4758676dbd313fdc214166596afe8e59939dd
CODER-2305 <Misterhayrulllo@gmail.com>
2021-04-30 22:58:00 +0500

    months

---
+++ app.js
@@ -0,0 +1,44 @@
+ var oy =+prompt('Tug\'ilgan oyingizni tanlang')
+switch (OYLAR) {
+         case 1:
+        alert('Siz Yanvar oyida tug\'ilgansiz va yanvar oyi qish faslida');
+        break;
+    case 2 :
+        alert('Siz Fevral oyida tug\'ilgansiz va Fevral oyi qish faslida');
+        break;
+    case 3 :
+        alert('Siz Mart oyida tug\'ilgansiz va Mart oyi Bahor faslida');
+        break;
+    case 4:
+        alert('Siz Aprel oyida tug\'ilgansiz va Aprel oyi Bahor faslida');
+        break;
+    case 5:
+        alert('Siz May oyida tug\'ilgansiz va May oyi Bahor faslida');
+        break;
+    case 6:
+        alert('Siz Iyun oyida tug\'ilgansiz va Iyun oyi Yoz faslida');
+        break;
+    case 7:
+        alert('Siz Iyul oyida tug\'ilgansiz va Iyul oyi Yoz faslida') ;
+        break;
+    case 8:
+        alert('Siz Avgust oyida tug\'ilgansiz va Avgust oyi Yoz faslida');
+        break;
+    case 9:
+        alert('Siz Sentabr oyida tug\'ilgansiz va Sentabr oyi Kuz faslida');
+        break;
+    case 10:
+        alert('Siz Oktabr oyida tug\'ilgansiz va Oktabr oyi Kuz faslida');
+        break;
+    case 11:
+        alert('Siz Noyabr oyida tug\'ilgansiz va Noyabr oyi Kuz faslida');
+        break;
+    case 12:
+        alert('Siz Dekabr  oyida tug\'ilgansiz va Dekabr oyi qish faslida');
+        break;
+    
+
+    default:
+     alert('bunday  oy faqat tushingizda boladi allo uygonamiz');
+        break;
+}
